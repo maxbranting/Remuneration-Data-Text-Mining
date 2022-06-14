9c5bcb6b341e4b7688c71f9930c2a9d41e40f7dd
maxbranting <brantingmax@gmail.com>
2022-06-14 02:08:32 +0200

    mm

--- textmining/Scripts/activate.bat
+++ textmining/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\maxim\Documents\GitHub\Remuneration-Data-Text-Mining\text-mining
+set VIRTUAL_ENV=C:\Users\maxim\Documents\GitHub\Remuneration-Data-Text-Mining\textmining
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(text-mining) %PROMPT%
+set PROMPT=(textmining) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(text-mining) 
+set VIRTUAL_ENV_PROMPT=(textmining) 
 
 :END
 if defined _OLD_CODEPAGE (
