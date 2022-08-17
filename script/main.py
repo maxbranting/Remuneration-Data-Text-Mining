9edef046fa4beebdab3039006493e38e05c3f677
maxbranting <brantingmax@gmail.com>
2022-08-17 21:36:09 +0200

    small changes

--- script/main.py
+++ script/main.py
@@ -6,39 +6,18 @@ import re
 import pandas as pd
 import pikepdf
 import camelot
+from .functions import pdf_tables_to_df
 from script.variables import pth, key_words, keywords_pay
-from functions import get_pages
+from functions import get_pages, pdf_tables_to_df
 
-pdf_dir = os.listdir(pth)
+#pdf_dir = os.listdir(pth)
 pdf_list = []
-for file in pdf_dir:
-    pdf_list.append(file)
+#for file in pdf_dir:
+#    pdf_list.append(file)
 
 
 get_pages(pdf_list)
 
 
 
-for title, pages in pdf_pages.items():
-    #list for documents that had no tables
-    notabpdfs = []
-    #empty list of dataframes
-    tempdfs = []
-    for page in pages:
-        #converting page to str for camelot read
-        cpage = str(page)
-        #reading in the tables in given pages
-        tables = camelot.read_pdf(pth + '/' + title, pages=cpage)
-        if len(tables)==0:
-            notabpdfs.append(title)
-        else:    
-            for i in range(len(tables)):
-                tempdf = tables[i].df
-                #deleting last 4 chars in title which is the ".pdf" part
-                tempdf['Institution_year'] = title[:-4]
-                # appending temporary dfs to a list
-                tempdfs.append(tempdf)
-                #concatanating all temporary dfs from the page
-                dfconc = pd.concat(tempdfs)
-                # concatanating newest version of  dftext by tables from the page
-                dftext = pd.concat([dftext, dfconc], axis = 0, ignore_index = True)
+pdf_tables_to_df()
